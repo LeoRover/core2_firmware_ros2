7f0427984cdb6ca93353f343e1584dc3127ccc1c
Błażej Sowa <bsowa123@gmail.com>
2023-01-10 15:32:54 +0100

    Use constexpr for compile-time constants

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -5,60 +5,60 @@
 #include "motor_controller.hpp"
 
 // Size of the heap memory used for micro-ROS entities
-const uint32_t UROS_HEAP_SIZE = 30000;
+constexpr uint32_t UROS_HEAP_SIZE = 30000;
 
 // Domain ID used for ROS communication
 constexpr size_t ROS_DOMAIN_ID = 0;
 
 // Name of the ROS node
-constexpr char ROS_NODE_NAME[] = "firmware";
+constexpr const char* ROS_NODE_NAME = "firmware";
 
 // Namespace of the ROS node
-constexpr char ROS_NAMESPACE[] = "";
+constexpr const char* ROS_NAMESPACE = "";
 
 // Number of encoder readings to remember when estimating the wheel velocity
-const uint32_t ENCODER_BUFFER_SIZE = 10;
+constexpr uint32_t ENCODER_BUFFER_SIZE = 10;
 
 // The period (in number of calls to the update() function) at which the battery
 // voltage is probed
-const uint8_t BATTERY_PROBE_PERIOD = 10;
+constexpr uint8_t BATTERY_PROBE_PERIOD = 10;
 
 // Number of battery voltage readings to average
-const uint32_t BATTERY_BUFFER_SIZE = 300;
+constexpr uint32_t BATTERY_BUFFER_SIZE = 300;
 
 // Informative LED GPIO
-const PinName LED_PIN = EXT_PIN1;
+constexpr PinName LED_PIN = EXT_PIN1;
 
 // The period (in milliseconds) between calls to the update() function
-const uint16_t UPDATE_PERIOD = 10;
+constexpr uint16_t UPDATE_PERIOD = 10;
 
 // The periods (in number of calls to the update() function) at which different
 // data is publihed on the ROS topics
-const uint8_t BATTERY_PUB_PERIOD = 10;
-const uint8_t JOINTS_PUB_PERIOD = 5;
-const uint8_t ODOM_PUB_PERIOD = 5;
-// const uint8_t IMU_PUB_PERIOD = 1;
+constexpr uint8_t BATTERY_PUB_PERIOD = 10;
+constexpr uint8_t JOINTS_PUB_PERIOD = 5;
+constexpr uint8_t ODOM_PUB_PERIOD = 5;
+// constexpr uint8_t IMU_PUB_PERIOD = 1;
 
 // Motor driver configurations
-const MotorConfiguration MOT_A_CONFIG = {
+constexpr MotorConfiguration MOT_A_CONFIG = {
     .driver_num = 0,
     .motor_num = MOT1,
     .reverse_polarity = false,
 };
 
-const MotorConfiguration MOT_B_CONFIG = {
+constexpr MotorConfiguration MOT_B_CONFIG = {
     .driver_num = 0,
     .motor_num = MOT2,
     .reverse_polarity = false,
 };
 
-const MotorConfiguration MOT_C_CONFIG = {
+constexpr MotorConfiguration MOT_C_CONFIG = {
     .driver_num = 1,
     .motor_num = MOT1,
     .reverse_polarity = true,
 };
 
-const MotorConfiguration MOT_D_CONFIG = {
+constexpr MotorConfiguration MOT_D_CONFIG = {
     .driver_num = 1,
     .motor_num = MOT2,
     .reverse_polarity = true,
@@ -69,7 +69,7 @@ extern MotorController MotB;
 extern MotorController MotC;
 extern MotorController MotD;
 
-const diff_drive_lib::DiffDriveConfiguration DD_CONFIG = {
+constexpr diff_drive_lib::DiffDriveConfiguration DD_CONFIG = {
     .wheel_FL_conf =
         {
             .motor = MotC,
