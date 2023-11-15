1ec66a99cbe49f7cebcadb4047f85975d761fe0a
Błażej Sowa <bsowa123@gmail.com>
2023-11-15 16:16:08 +0100

    Fix default firmware params

--- include/parameters.hpp
+++ include/parameters.hpp
@@ -9,16 +9,16 @@ struct Parameters : diff_drive_lib::RobotParams {
   Parameters() {
     // Wheel
     wheel_encoder_resolution = 878.4F;
-    wheel_torque_constant = 1.0F;
+    wheel_torque_constant = 1.17647F;
     wheel_pid_p = 0.0F;
     wheel_pid_i = 0.005F;
     wheel_pid_d = 0.0F;
     wheel_pwm_duty_limit = 100.0F;
 
     robot_wheel_radius = 0.0625F;
-    robot_wheel_separation = 0.33F;
-    robot_wheel_base = 0.3052;
-    robot_angular_velocity_multiplier = 1.91F;
+    robot_wheel_separation = 0.358F;
+    robot_wheel_base = 0.3052F;
+    robot_angular_velocity_multiplier = 1.76F;
     robot_input_timeout = 500;
   }
 
